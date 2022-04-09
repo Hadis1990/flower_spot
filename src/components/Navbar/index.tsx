44d1d92f9b159c86ceb212cca299988330f5c435
Me <me@example.com>
2022-04-09 15:10:07 +0200

    finish basic navbar styling

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import navbarLogo from "../../assets/images/cherry-blossom.png";
+
 import "./index.scss";
 
 export default () => (
   <nav id="main-navbar">
     <div className="section-1">
-      <Link to="/home">FlowrSpot</Link>
+      <img src={navbarLogo} className="logo" />
+      <Link to="/home" className="pink-font">
+        FlowrSpot
+      </Link>
     </div>
 
     <div className="section-2">
-      <Link to="/home">Flowers</Link>
-      <Link to="/latest_sightings">Latest Sightings</Link>
-      <Link to="/favorites">Favorites</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">New Account</Link>
+      <Link to="/home" className="gray-font">
+        Flowers
+      </Link>
+      <Link to="/latest_sightings" className="gray-font">
+        Latest Sightings
+      </Link>
+      <Link to="/favorites" className="gray-font">
+        Favorites
+      </Link>
+      <Link to="/login" className="pink-font">
+        Login
+      </Link>
+      <Link to="/register" className="white-font">
+        New Account
+      </Link>
     </div>
   </nav>
 );
