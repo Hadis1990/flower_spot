60d0165b04df1026a68e3ebd0ccf9ac1aeb2e563
Me <me@example.com>
2022-04-09 15:19:48 +0200

    setup basic wallpaper component

---
+++ src/components/Wallpaper/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import wallpaperImg from "../../assets/images/wallpaper.webp";
+
+export default () => (
+  <div style={{ backgroundImage: `url(${wallpaperImg})` }}>
+    <h1>Discover flowers around you</h1>
+    <h2>Explore between more than 8.427 sightings</h2>
+  </div>
+);
